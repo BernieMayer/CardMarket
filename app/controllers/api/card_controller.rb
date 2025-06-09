e08de09f3a081eeb13dd947649f5611e6e942a23
Bernie Mayer <berniemayer94@gmail.com>
2025-06-08 19:27:16 -0600

    Add TODO

--- app/controllers/api/card_controller.rb
+++ app/controllers/api/card_controller.rb
@@ -1,6 +1,8 @@
 class Api::CardController < ApplicationController
 
   def index 
+
+    # TODO: Implement full logic of renting cards
     @card = Card::first
     render json: { suit: @card.suit, card: @card.card }
   end
